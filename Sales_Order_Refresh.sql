bb74d355b3cd2b43cd60c0820bdece93a8a9ac40
Ukachuku <57579782+Ukachuku@users.noreply.github.com>
2020-05-11 11:41:31 -0700

    Create Sales_Order_Refresh.sql
    situations where the temp table does not drop

---
+++ Sales_Order_Refresh.sql
@@ -0,0 +1,4 @@
+IF EXISTS(SELECT name FROM [Sales_Reporting].sys.tables   WHERE name = N'Shipped_11temp') drop table SALES_REPORTING.dbo.Shipped_11temp
+
+IF EXISTS(SELECT name FROM [Sales_Reporting].sys.tables   WHERE name = N'Shipped_22temp') drop table SALES_REPORTING.dbo.Shipped_22temp
+
